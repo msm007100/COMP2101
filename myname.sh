62773675a7edf370e1358594589a127cb1d1c924
msm007100 <msm-says@hotmail.com>
2018-09-25 13:48:24 -0700

    added

---
+++ myname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export MYNAME="Morgan"
+mytitle="Mr."
+today=$(date)
+hostname=$(hostname)
+#echo $mytitle $MYNAME
+
+echo "Welcome to planet $hostname $mytitle $MYNAME"
+echo "Today is $today"
